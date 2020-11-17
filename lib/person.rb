7b685b46c11c2501892aac121e01dcb9de4f35f4
danabliss <danajackson2@gmail.com>
2020-11-17 15:05:46 -0800

    commit tree 4e0088b805f1e01761c8a040d801c317e13409ab

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def name
+        @name
+    end
+    def name=(x)
+        @name = x
+    end
+    def job
+        @job
+    end
+    def job=(y)
+        @job = y
+    end
+end    
