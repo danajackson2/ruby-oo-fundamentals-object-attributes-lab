125ae52357c9aeb4c455f404938c896a492beffc
danabliss <danajackson2@gmail.com>
2020-11-17 15:06:47 -0800

    obj attributes lab

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    def name
+        @name
+    end
+    def name=(x)
+        @name = x
+    end
+    def breed
+        @breed
+    end
+    def breed=(y)
+        @breed = y
+    end
+end    
